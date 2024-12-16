7c8164e2048021f1eec8622d12deb758448b0da0
mokotok <mokotok@protonmail.com>
2024-12-16 12:56:24 +1000

    The skeleton

---
+++ update-btc-csv.py
@@ -0,0 +1,37 @@
+file = "filepath"
+
+def check_for_file() -> None:
+    if file == True:
+        delete_record()
+        get_results(check_dates())
+    else:
+        create_file()
+    return
+
+
+def create_file():
+    #Creates file
+    get_results(1000)
+    return
+
+
+def get_results(results: int) -> int:
+    results = 69
+    return results 
+
+
+def delete_record() -> None:
+    return
+
+
+def check_dates() -> int:
+    return 69
+
+
+def main() -> None:
+    check_for_file()
+
+
+if __name__ == "__main__":
+    main()
+    print("Ghost program")
